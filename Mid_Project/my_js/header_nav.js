822afdcc1a6b91782708299c61ff0e9d7bc62bac
guswn <guswns3371@gmail.com>
2019-05-07 17:50:18 +0900

    Initial commit

---
+++ Mid_Project/my_js/header_nav.js
@@ -0,0 +1,64 @@
+var str = "<header class=\"header\">\n" +
+    "    <div class=\"header_line\">\n" +
+    "\n" +
+    "        <div class=\"menu\">\n" +
+    "            <button id=\"menu_btn\" title=\"menu\" type=\"button\" onclick=\"menu_btn();\">\n" +
+    "                <img src=\"../my_file/icon/menu.png\">\n" +
+    "            </button>\n" +
+    "        </div>\n" +
+    "        <div class=\"logo\">\n" +
+    "            <a id=\"logo\"  href=\"https://www.naver.com\" title=\"Go to Home\">\n" +
+    "                <img src=\"../my_file/icon/logo.png\" alt=\"logo\">\n" +
+    "            </a>\n" +
+    "        </div>\n" +
+    "        <div class=\"search\">\n" +
+    "            <input type=\"text\" placeholder=\"Search\" title=\"search\">\n" +
+    "            <button title=\"search button\" type=\"button\"><img src=\"../my_file/icon/search.png\"></button>\n" +
+    "        </div>\n" +
+    "        <div class=\"user_control\">\n" +
+    "            <a id=\"user_icon\" href=\"\" title=\"user account\">\n" +
+    "                <img src=\"../my_file/icon/userimg.jpg\" alt=\"user\" >\n" +
+    "            </a>\n" +
+    "        </div>\n" +
+    "\n" +
+    "    </div>\n" +
+    "\n" +
+    "\n" +
+    "\n" +
+    "</header>\n" +
+    "<nav class=\"nav\">\n" +
+    "    <div class=\"fast\">\n" +
+    "        <ul>\n" +
+    "            <a href=\"../my_html/mainpage.html\"><li><img src=\"../my_file/icon/home.png\" alt=\"home\"><span>홈</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"popular\"><span>인기</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/news.png\" alt=\"news\"><span>뉴스</span></li></a>\n" +
+    "        </ul>\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"category\">\n" +
+    "        <span class=\"category_name\">라이브러리</span>\n" +
+    "        <ul>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>1</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>2</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>3</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>4</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>5</span></li></a>\n" +
+    "        </ul>\n" +
+    "    </div>\n" +
+    "\n" +
+    "    <div class=\"category\">\n" +
+    "        <span class=\"category_name\">구독</span>\n" +
+    "        <ul>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>1</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>1</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>1</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>1</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>1</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>1</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>1</span></li></a>\n" +
+    "            <a href=\"\"><li><img src=\"../my_file/icon/popular.png\" alt=\"\"><span>1</span></li></a>\n" +
+    "        </ul>\n" +
+    "    </div>\n" +
+    "</nav>";
+
+document.write(str);
