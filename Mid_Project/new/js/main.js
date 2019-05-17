65c5ccbdccad0bc058fec69cf099b4d0352b2a46
guswn <guswns3371@gmail.com>
2019-05-18 02:44:32 +0900

    web2

---
+++ Mid_Project/new/js/main.js
@@ -0,0 +1,11 @@
+$("#more_nav_btn").click(function () {
+    const more_nav = document.getElementById('more_nav');
+    if (more_nav.style.display === 'none') {
+        $("#more_nav_btn").html("<span>접기 </span><img src=\"file/icon/up_arr.png\">");
+        more_nav.style.display = 'block';
+    }else {
+        $("#more_nav_btn").html("<span>더보기 </span><img src=\"file/icon/down_arr.png\">");
+        more_nav.style.display = 'none';
+    }
+    // more_nav.style.display === 'none' ? more_nav.style.display = 'block' : more_nav.style.display = 'none';
+});
