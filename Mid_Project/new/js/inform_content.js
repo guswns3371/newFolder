7f52c1588b7c45afdf2095b9bb65907502644a1f
90531sky <90531sky@naver.com>
2019-06-06 18:27:04 +0900

    seokgyulast

--- Mid_Project/new/js/inform_content.js
+++ Mid_Project/new/js/inform_content.js
@@ -1,6 +1,32 @@
-function doDisplay(){
-    var con = document.getElementsByName("H");
-    if(con.onclick){
-        document.getElementsByClassName("content1").style.display = 'block';
+function doDisplayH(){
+    const Hcon = document.getElementById("Hcon");
+    if(Hcon.style.display == 'none'){
+        Hcon.style.display = 'block';
+		Scon.style.display = 'none';
+		Ycon.style.display = 'none';
+    }else{
+        Hcon.style.display = 'none';
+    }
+}
+
+function doDisplayS(){
+    const Scon = document.getElementById("Scon");
+    if(Scon.style.display == 'none'){
+        Scon.style.display = 'block';
+		Hcon.style.display = 'none';
+		Ycon.style.display = 'none';
+    }else{
+        Scon.style.display = 'none';
+    }
+}
+
+function doDisplayY(){
+    const Ycon = document.getElementById("Ycon");
+    if(Ycon.style.display == 'none'){
+        Ycon.style.display = 'block';
+		Hcon.style.display = 'none';
+		Scon.style.display = 'none';
+    }else{
+        Ycon.style.display = 'none';
     }
 }
