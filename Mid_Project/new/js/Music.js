fcd207c864ac6522c410502fe8102389dde340c0
guswn <guswns3371@gmail.com>
2019-06-06 20:24:45 +0900

    web2

--- Mid_Project/new/js/Music.js
+++ Mid_Project/new/js/Music.js
@@ -1,5 +1,7 @@
 {
     domestic();
+    showimg();
+    Music_domestic();
 }
 
 function domestic()
@@ -36,4 +38,48 @@ function foreign() {
     var music_list = document.getElementById("music_list");
     music_list.innerHTML ="";
     music_list.innerHTML =str;
+}
+function showimg(){
+    var str="";
+    for(let c=0;c<3;c++)
+    {
+        str+="          <div class=\"commercial_content\">\n"+
+            "    <img src=\"file/img/Music/commercial("+(c+1)+").png \"height=\"150px\" width=\"300px\">"+
+            "       </div>";
+    }
+    var commercial_list=document.getElementById("commercial_list");
+    commercial_list.innerHTML="";
+    commercial_list.innerHTML=str;
+}
+function displaynone(){
+    var commercial_list=document.getElementById("commercial_list");
+    commercial_list.innerHTML="";
+}
+function Music_domestic(){
+    var str="";
+    var music_artist = ['스트릿건즈(Street guns)','산들(B1A4)','마레','이하이',
+        '투모로우바이투게더'];
+    var music_name=['오래된 무언가','날씨 좋은 날','무슨 소용이 있나','누구 없소(NO ONE)','별의 낮잠'];
+    for(let i=0;i<5;i++)
+    {
+        str += "<div class=\"music_video_box\" >\n" +
+            "            <img src=\"file/img/Music/music_video("+(i+1)+").png\" ><p>"+music_artist[i]+"<br>"+music_name[i]+"</p>\n" +
+            "</div>";
+    }
+    const music_video = document.getElementById("music_video");
+    music_video.innerHTML=str;
+}
+function Music_foreign(){
+    var str="";
+    var music_artist = ['Iceyys','IIVU(이브)','DJ Snake,shec..','Kevin Morby',
+        'Ciara'];
+    var music_name=['Volatile(Radio Edit.)','Enzo(Feat. Offset,21,Savage,Gucci Mane','OMG Rock N Roll','누구 없소(NO ONE)','Thinkin Bout you'];
+    for(let i=0;i<5;i++)
+    {
+        str += "<div class=\"music_video_box\" >\n" +
+            "            <img src=\"file/img/Music/music_video_foreign("+(i+1)+").png\" ><p>"+music_artist[i]+"<br>"+music_name[i]+"</p>\n" +
+            "</div>";
+    }
+    var music_video_foreign=document.getElementById("music_video");
+    music_video_foreign.innerHTML=str;
 }
